4aed44e14d47754709113216481fce2c8097bb53
Daniele Molinari <danieleuni@Air-di-Daniele.home>
2024-01-31 17:11:30 +0100

    Aggiunta logica repository e IRepository del servizio pokedex

--- microservizi/PokedexService/PokedexService.Repository/Abstraction/IRepository.cs
+++ microservizi/PokedexService/PokedexService.Repository/Abstraction/IRepository.cs
@@ -1,7 +1,15 @@
+using PokedexService.Repository.Model;
+
 namespace PokedexService.Repository.Abstraction
 {
     public interface IRepository
     {
         
+        Task AddPokemonAsync(Pokemon pokemon, CancellationToken cancellationToken = default);
+        Task<Pokemon> GetPokemonByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<Pokemon> GetPokemonByNameAsync(string name, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Pokemon>> GetAllPokemons(CancellationToken cancellationToken = default);
+
+
     }
 }
--- microservizi/PokedexService/PokedexService.Repository/Repository.cs
+++ microservizi/PokedexService/PokedexService.Repository/Repository.cs
@@ -1,9 +1,50 @@
 ﻿using PokedexService.Repository.Abstraction;
+using PokedexService.Repository.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace PokedexService.Repository
 {
     public class Repository : IRepository
     {
+
+        private PokedexServiceDbContext _PokedexServiceDbContext;
+        public Repository(PokedexServiceDbContext pokedexServiceDbContext)
+        {
+            _PokedexServiceDbContext = pokedexServiceDbContext;
+        }
+
+        public async Task AddPokemonAsync(Pokemon pokemon, CancellationToken cancellationToken = default)
+        {
+            if(pokemon == null)
+                throw new NullReferenceException();
+
+            await _PokedexServiceDbContext.Pokemons.AddAsync(pokemon, cancellationToken);
+        }
+
+        public async Task<Pokemon> GetPokemonByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            Pokemon? result = await _PokedexServiceDbContext.Pokemons.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+            if(result == null)
+                throw new NullReferenceException();
+
+            // Il risultato può essere null
+            return result;
+        }
+
+        public async Task<Pokemon> GetPokemonByNameAsync(string name, CancellationToken cancellationToken = default)
+        {
+            Pokemon? result = await _PokedexServiceDbContext.Pokemons.FirstOrDefaultAsync(x => x.PokemonName == name, cancellationToken);
+
+            if(result == null)
+                throw new NullReferenceException();
+                
+            // Il risultato può essere null
+            return result;
+        }
+
+        public async Task<IEnumerable<Pokemon>> GetAllPokemons(CancellationToken cancellationToken = default) => await _PokedexServiceDbContext.Pokemons.ToListAsync(cancellationToken);
+
 
     }
     
