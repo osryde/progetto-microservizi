23b9a823c2e72e368e6b7cfb088acdd4b41888e7
osryde <danielemantova2001@gmail.com>
2024-07-25 14:27:51 +0200

    Code review

--- microservizi/PokemonTrainerService/PokemonTrainerService.ClientHttp/ClientHttp.cs
+++ microservizi/PokemonTrainerService/PokemonTrainerService.ClientHttp/ClientHttp.cs
@@ -1,5 +1,16 @@
-﻿namespace PokemonTrainerService.ClientHttp;
-public class Class1
-{
+﻿using PokemonTrainerService.ClientHttp.Abstraction;
+using PokemonTrainerService.Repository.Model;
+using System.Net.Http.Json;
 
+namespace PokemonTrainerService.ClientHttp
+{
+    public class ClientHttp : IClientHttp
+    {
+        private readonly HttpClient _httpClient;
+        public ClientHttp(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+    }
 }
