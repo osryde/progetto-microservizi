749431d49d3add77a3ae3fb618dbbecab52de3cb
osryde <danielemantova2001@gmail.com>
2024-07-25 15:32:36 +0200

    Implementazione base di Business (Da finire con Kafka)

--- microservizi/PokemonTrainerService/PokemonTrainerService.Business/Abstraction/IBusiness.cs
+++ microservizi/PokemonTrainerService/PokemonTrainerService.Business/Abstraction/IBusiness.cs
@@ -1,7 +1,13 @@
+using PokemonTrainerService.Repository;
+using PokemonTrainerService.Repository.Model;
+
 namespace PokemonTrainerService.Business.Abstraction
 {
     public interface IBusiness 
     {
+        Task<IEnumerable<Item>> ListaZaino(CancellationToken cancellationToken = default);
+        Task<Item> AggiungiOggetto(Item item, CancellationToken cancellationToken = default);
+        Task<String> CreaSquadraCasuale(CancellationToken cancellationToken = default);
 
     }
 }
--- microservizi/PokemonTrainerService/PokemonTrainerService.Business/Business.cs
+++ microservizi/PokemonTrainerService/PokemonTrainerService.Business/Business.cs
@@ -1,10 +1,34 @@
 ﻿using PokemonTrainerService.Business.Abstraction;
+using PokemonTrainerService.Repository.Model;
+using PokemonTrainerService.Repository.Abstraction;
+
 
 namespace PokemonTrainerService.Business
 {
     public class Business : IBusiness
-    {
+    {   
 
+        private IRepository repo;
+        //private readonly PokemonTrainerService.ClientHttp.Abstraction.IClientHttp _clientHttp;
+        public Business(IRepository repository)
+        {
+            repo = repository;
+        }
+
+        public async Task<IEnumerable<Item>> ListaZaino(CancellationToken cancellationToken = default)
+        {
+            IEnumerable<Item> items = await repo.GetAllItems();
+            return items;
+        }
+        public async Task<Item> AggiungiOggetto(Item item, CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException(); // DA FARE
+        }
+
+        public async Task<String> CreaSquadraCasuale(CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException(); // DA FARE 
+        }
     }
 
 }
