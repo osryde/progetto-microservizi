ae760bcfa908ccae8106e8853c357bb4e35b618e
osryde <danielemantova2001@gmail.com>
2024-07-26 12:02:12 +0200

    Aggiunto kafka (ancora da modificare)

---
+++ microservizi/GlobalUtility/Utility.Kafka/Abstractions/Clients/IProducerClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Utility.Kafka.Abstractions.Clients;
+
+public interface IProducerClient : IDisposable {
+
+    Task ProduceAsync(string topic, string message, CancellationToken cancellationToken = default);
+
+    Task ProduceAsync(string topic, int partition, string message, CancellationToken cancellationToken = default);
+
+}
